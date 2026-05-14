24e5ff2c91af2b257848641d9655d59034c70fa2
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2026-05-14 22:24:03 +0200

    Add overlap computation functionality (#7)

--- src/bindings/module.cxx
+++ src/bindings/module.cxx
@@ -1,5 +1,6 @@
 #include "blocking.hxx"
 #include "graph.hxx"
+#include "ground_truth.hxx"
 #include "segmentation.hxx"
 #include "utils.hxx"
 
@@ -11,6 +12,7 @@ NB_MODULE(_core, m) {
     m.doc() = "C++ extension module for bioimage_cpp.";
     bioimage_cpp::bindings::bind_blocking(m);
     bioimage_cpp::bindings::bind_graph(m);
+    bioimage_cpp::bindings::bind_ground_truth(m);
     bioimage_cpp::bindings::bind_segmentation(m);
     bioimage_cpp::bindings::bind_utils(m);
 }
