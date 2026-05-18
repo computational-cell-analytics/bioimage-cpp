53a4bed28c99307ebf0fffd910aee6a4552bbd2c
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2026-05-18 21:37:15 +0200

    Update test matrix and doc

--- src/bioimage_cpp/__init__.py
+++ src/bioimage_cpp/__init__.py
@@ -1,4 +1,26 @@
-"""Dependency-light bioimage analysis algorithms backed by C++."""
+"""`bioimage_cpp` implements functionality needed for image procesing in C++.
+It generates light-weight python bindings with nanobind with minimal dependencies to enable distribution via pip.
+
+The main goal of this library is to provide functionality that is missing from scipy or scikit-image or
+to provide more performant versions of functionality from these libraries.
+
+The functionality implemented here bundles and improves algorithms etc. from:
+- [affogato](https://github.com/constantinpape/affogato)
+- [fastfilters](https://github.com/sciai-lab/fastfilters)
+- [nifty](https://github.com/DerThorsten/nifty)
+- [vigra](https://github.com/ukoethe/vigra)
+
+The goal is to provide the functionality within a single library and via pip as well as conda.
+
+**Warning:** This library was written mainly by coding agents (claude code and openai codex).
+It is not very thoroughly tested and may contain bugs.
+
+## Installation
+
+TODO: document once on pip / conda
+
+.. include:: ../../MIGRATION_GUIDE.md
+"""
 
 from ._version import __version__
 from . import affinities
